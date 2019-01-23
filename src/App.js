7467235d81c8b0ad520559fba221220c06ffcd6f
Manit Kant <manit_k@HCL.COM>
2019-01-23 19:58:52 +0530

    working webcomponent integrated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import '@vaadin/vaadin-date-picker';
 
 class App extends Component {
   render() {
     return (
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <p>
+      //       Edit <code>src/App.js</code> and save to reload.
+      //     </p>
+      //     <a
+      //       className="App-link"
+      //       href="https://reactjs.org"
+      //       target="_blank"
+      //       rel="noopener noreferrer"
+      //     >
+      //       Learn React
+      //     </a>
+      //   <div>
+      //     <vaadin-date-picker label="When were you born?"></vaadin-date-picker>
+      //   </div>  
+      //   </header>
+      //   {/* <body>
+      //     Body
+      //   </body> */}
+      // </div>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <vaadin-date-picker label="When were you born?"></vaadin-date-picker>
       </div>
     );
   }
